4cea5dc4327d5d72bc9965c3e9968bac47c35c5a
GalanthusPlicatus <galanthusplicatus@gmail.com>
2016-05-21 16:45:05 +0530

    Admin page

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
 
 # Register your models here.
+from base.models import Dream, Budget
+
+
+class DreamAdmin(admin.ModelAdmin):
+    def creator(self, obj):
+        if hasattr(obj.created_by, 'username'):
+            return obj.created_by.username
+        else:
+            return 'None'
+    creator.short_description = 'Store created by'
+
+    def save_model(self, request, obj, form, change):
+        if obj.id:
+            obj.updated_by = request.user
+        else:
+            obj.created_by = request.user
+        obj.save()
+    list_display = (
+            'name',
+            'visibility',
+            'status',
+            'total_budget',
+            'created_by',
+            'updated_by'
+    )
+admin.site.register(Dream, DreamAdmin)
+
+
+class BudgetAdmin(admin.ModelAdmin):
+    list_display = ('get_dream_name', 'get_budget_type', 'amount')
+
+    def get_dream_name(self, obj):
+        return obj.dream.name
+
+    def get_budget_type(self, obj):
+        return obj.budget_type.name
+
+
+admin.site.register(Budget, BudgetAdmin)
