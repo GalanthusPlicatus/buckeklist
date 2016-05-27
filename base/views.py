cb339beaa8895eae705d8a91a2dcd61a38ab7fd3
Saranya <galanthusplicatus@gmail.com>
2016-05-27 22:32:25 +0530

    View for certain plan

--- base/views.py
+++ base/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render
 from rest_framework import generics
 
 from models import Dream
-from serializer import DreamSerializer
+from serializer import DreamSerializer, BudgetSerializer, BudgetTypeSerializer
 # Create your views here.
 
 
@@ -14,3 +14,9 @@ class BucketListApi(generics.ListCreateAPIView):
     """To list all dreams"""
     queryset = Dream.objects.all()
     serializer_class = DreamSerializer
+
+# class BucketApi(generics.ListCreateAPIView):
+#     """To list all dreams"""
+#     queryset = Dream.objects.all()
+#     serializer_class = DreamSerializer    
+
