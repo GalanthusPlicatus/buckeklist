15ab8de6bb7614daeb913c968736c1b718ec666b
GalanthusPlicatus <galanthusplicatus@gmail.com>
2016-06-10 21:47:24 +0530

    list of new test cases

--- base/tests/test_views.py
+++ base/tests/test_views.py
@@ -10,10 +10,7 @@ from base.models import Dream
 
 
 class FirstTestForViewTests(APITestCase):
-
-    def setUp(self):
-        self.client = APIClient()
-
+    # Simple test to check dream object is created.
     def test_create_dream(self):
         url = reverse('api:dream_list')
         payload = {
@@ -25,5 +22,13 @@ class FirstTestForViewTests(APITestCase):
         # print data
         response = self.client.post(url, payload)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        # self.assertEqual(Dream.objects.count(), 1)
-        # self.assertEqual(Dream.objects.get().name, 'buy bucket')
+        self.assertEqual(Dream.objects.count(), 1)
+        self.assertEqual(Dream.objects.get().name, 'buy bucket')
+
+        """test cases:
+            1. check user is created
+            2. is user authenticated
+            3. created_by should not be null
+            4. when dream object is created status and visibility should
+            have default values
+        """
